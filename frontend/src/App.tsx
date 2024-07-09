86215ba7012afaf909467f7985f5061902fd48b7
Davidmoreen <davidmoreen@gmail.com>
2024-07-09 01:03:51 -0400

    Show auth form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
 import '@mantine/core/styles.css'
 import ReactDOM from 'react-dom/client'
-import { useActions, useValues } from 'kea'
-import { appLogic } from './logic/appLogic'
 import { createTheme, Container, MantineProvider } from '@mantine/core'
 import { AuthenticationForm } from './scenes/auth/AuthenticationForm'
 
-const theme = createTheme({
-  fontFamily: '"Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif',
-})
-
 export function App() {
-  const { count } = useValues(appLogic)
-  const { setCount } = useActions(appLogic)
+  const theme = createTheme({
+    fontFamily: '"Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif'
+  })
 
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={theme} defaultColorScheme="auto">
       <Container size="responsive">
         <AuthenticationForm />
       </Container>
-    </MantineProvider>
+    </MantineProvider >
   )
 }
 
--- frontend/src/scenes/auth/AuthenticationForm.tsx
+++ frontend/src/scenes/auth/AuthenticationForm.tsx
@@ -1,6 +1,6 @@
 import { useToggle, upperFirst } from '@mantine/hooks'
 import { useForm } from '@mantine/form'
-import { Anchor, Button, Checkbox, Group, Paper, PaperProps, Stack, Text, TextInput } from '@mantine/core'
+import { Anchor, Button, Checkbox, Group, Paper, PaperProps, Stack, Title, TextInput, Center } from '@mantine/core'
 
 
 export function AuthenticationForm(props: PaperProps) {
@@ -20,59 +20,61 @@ export function AuthenticationForm(props: PaperProps) {
     })
 
     return (
-        <Paper radius="md" p="xl" withBorder {...props}>
-            <Text size="lg" fw={500}>
-                Welcome to Paywalls, {type}
-            </Text>
+        <Center>
+            <Paper shadow="sm" mt="xl" radius="md" p="xl" withBorder {...props}>
+                <Title order={2}>
+                    {upperFirst(type)}
+                </Title>
 
-            <form onSubmit={form.onSubmit(() => { })}>
-                <Stack>
-                    {type === 'register' && (
+                <form onSubmit={form.onSubmit(() => { })}>
+                    <Stack>
+                        {type === 'register' && (
+                            <TextInput
+                                label="Name"
+                                placeholder="Your name"
+                                value={form.values.name}
+                                onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+                                radius="md"
+                            />
+                        )}
+
                         <TextInput
-                            label="Name"
-                            placeholder="Your name"
-                            value={form.values.name}
-                            onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+                            required
+                            label="Email"
+                            placeholder="hello@paywalls.io"
+                            value={form.values.email}
+                            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+                            error={form.errors.email && 'Invalid email'}
                             radius="md"
                         />
-                    )}
 
-                    <TextInput
-                        required
-                        label="Email"
-                        placeholder="hello@paywalls.io"
-                        value={form.values.email}
-                        onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
-                        error={form.errors.email && 'Invalid email'}
-                        radius="md"
-                    />
-
-                    <TextInput
-                        required
-                        label="Password"
-                        placeholder="Your password"
-                        value={form.values.password}
-                        onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
-                        error={form.errors.password && 'Password should include at least 6 characters'}
-                        radius="md"
-                    />
-
-                    {type === 'register' && (
-                        <Checkbox
-                            label="I accept terms and conditions"
-                            checked={form.values.terms}
-                            onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+                        <TextInput
+                            required
+                            label="Password"
+                            placeholder="Your password"
+                            value={form.values.password}
+                            onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+                            error={form.errors.password && 'Password should include at least 6 characters'}
+                            radius="md"
                         />
-                    )}
-                </Stack>
 
-                <Group justify="space-between" mt="xl">
-                    <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
-                        {type === 'register' ? 'Already have an account? Login' : 'Don’t have an account? Register'}
-                    </Anchor>
-                    <Button type="submit">{upperFirst(type)}</Button>
-                </Group>
-            </form>
-        </Paper>
+                        {type === 'register' && (
+                            <Checkbox
+                                label="I accept terms and conditions"
+                                checked={form.values.terms}
+                                onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+                            />
+                        )}
+                    </Stack>
+
+                    <Group justify="space-between" mt="xl">
+                        <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
+                            {type === 'register' ? 'Already have an account? Login' : 'Don’t have an account? Register'}
+                        </Anchor>
+                        <Button type="submit">{upperFirst(type)}</Button>
+                    </Group>
+                </form>
+            </Paper>
+        </Center>
     )
 }
