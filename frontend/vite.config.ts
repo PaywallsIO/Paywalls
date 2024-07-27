a7ed5e6d8a8f3adcc3cdc9bf9715addfb53b6c58
Davidmoreen <davidmoreen@gmail.com>
2024-07-26 23:07:04 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  publicDir: "../public",
-  root: "./src",
-  envDir: "../",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
-  server: {
-    host: "0.0.0.0"
-  }
+  plugins: [react()]
 })
