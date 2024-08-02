9b31100af9417f3c0a8f9ba7140e941bcff72eb9
Davidmoreen <davidmoreen@gmail.com>
2024-08-02 18:31:04 -0400

    Add Dockerfile

--- frontend/src/scenes/paywalls/data/PaywallsApiClient.ts
+++ frontend/src/scenes/paywalls/data/PaywallsApiClient.ts
@@ -11,8 +11,8 @@ interface PaywallsApiClientInterface {
 export type Paywall = {
     id: number
     name: string
-    created_at: string
-    updated_at: string
+    created_at: Date
+    updated_at: Date
     content: Object
 }
 
